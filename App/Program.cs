7dcd8fbb87846dcc2f254022713d46679809403a
nillerdata <eaa24nlgl@students.eaaa.dk>
2026-04-08 11:13:39 +0200

    Initial setup with EF, PostgreSQL, Docker and migrations

---
+++ App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Data.Context;
+using Domain.Entities;
+
+using var context = new AppDbContextFactory().CreateDbContext(null);
+
+// INSERT
+context.Users.Add(new User { Name = "Choomba" });
+context.SaveChanges();
+
+// READ
+var users = context.Users.ToList();
+
+foreach (var user in users)
+{
+    Console.WriteLine(user.Name);
+}
---
+++ Data/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    { 
+    }
+}
