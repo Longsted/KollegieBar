b5771cfa35c62db3df6fb7df19f2cf2006ade888
Niels <eaa24nlgl@students.eaaa.dk>
2026-04-08 11:25:59 +0200

    Initial setup with EF, PostgreSQL, Docker and migrations (#2)

---
+++ Data/Context/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5433;Username=postgres;Password=1234;Database=appdb"
+        );
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
