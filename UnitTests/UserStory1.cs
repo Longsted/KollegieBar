fd748fdb393246cbc9d5e21ea6c4357c0e2c6750
kevin <eaa24ksra@students.eaaa.dk>
2026-04-09 11:43:59 +0200

    Revamped tests to use XUnit, also made UserStory2 and mock models to be used for the tests so a new mock isnt needed for every test.

--- UnitTests/UserStory1.cs
+++ UnitTests/UserStory1.cs
@@ -1,5 +1,45 @@
-﻿namespace UnitTests;
+﻿using Xunit;
 
-internal class UserStory1
+namespace UnitTests
 {
+    public class UserStory1Tests
+    {
+        // Tester at lagerbeholdningen reduceres med 1 når bartenderen registrerer et salg
+        [Fact]
+        public void RegisterSale_ReducesStock_WhenUserRoleIsBartender()
+        {
+            var bartender = new User { Role = UserRole.Bartender };
+            var product = new Product
+            {
+                Name = "Beer",
+                Stock = 10,
+                Type = DrinkType.Beer,
+                Price = 25.0m
+            };
+
+            var inventoryService = new InventoryService();
+            inventoryService.RegisterSale(product);
+
+            Assert.Equal(9, product.Stock);
+        }
+
+        // Tester at lagerbeholdningen IKKE reduceres når rollen er null (ikke logget ind)
+        [Fact]
+        public void RegisterSale_DoesNotReduceStock_WhenUserRoleIsNull()
+        {
+            var noRoleUser = new User { Role = null };
+            var product = new Product
+            {
+                Name = "Beer",
+                Stock = 10,
+                Type = DrinkType.Beer,
+                Price = 25.0m
+            };
+
+            var inventoryService = new InventoryService();
+            inventoryService.RegisterSale(product);
+
+            Assert.Equal(10, product.Stock);
+        }
+    }
 }
---
+++ UnitTests/UserStory2.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class UserStory2Tests
+    {
+        // Tester at statistik over salget sendes når bestyrelsesmedlemmet har korrekt rolle
+        [Fact]
+        public void GetSalesStatistics_ReturnsData_WhenUserRoleIsBoardMember()
+        {
+            var boardMember = new User { Role = UserRole.BoardMember };
+
+            var salesData = new List<Product>
+            {
+                new Product { Name = "Beer", Stock = 10, Type = DrinkType.Beer, Price = 25.0m },
+                new Product { Name = "Cider", Stock = 5, Type = DrinkType.Cider, Price = 30.0m }
+            };
+
+            var statisticsService = new StatisticsService(salesData);
+            var result = statisticsService.GetSalesStatistics();
+
+            Assert.NotEmpty(result);
+        }
+
+        // Tester at statistik IKKE sendes når rollen er null (ikke logget ind)
+        [Fact]
+        public void GetSalesStatistics_ReturnsEmpty_WhenUserRoleIsNull()
+        {
+            var noRoleUser = new User { Role = null };
+
+            var salesData = new List<Product>
+            {
+                new Product { Name = "Beer", Stock = 10, Type = DrinkType.Beer, Price = 25.0m },
+                new Product { Name = "Cider", Stock = 5, Type = DrinkType.Cider, Price = 30.0m }
+            };
+
+            var statisticsService = new StatisticsService(salesData);
+            var result = statisticsService.GetSalesStatistics();
+
+            Assert.Empty(result);
+        }
+    }
+}
