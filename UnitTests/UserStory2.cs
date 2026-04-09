04458bbeebb435ad1c676d15091f3c70c0a84fe9
Kevin547-ux <eaa24ksra@students.eaaa.dk>
2026-04-09 12:06:11 +0200

    Revamped tests to use XUnit, also made UserStory2 and mock models to be used for the tests so a new mock isnt needed for every test. (#15)
    Revamped tests to use XUnit, also made UserStory2 and mock models to be used for the tests so a new mock isnt needed for every test. (#15)

--- UnitTests/UserStory2.cs
+++ UnitTests/UserStory2.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Xunit;
 
 namespace UnitTests
 {
-    internal class UserStory2
+    public class UserStory1Tests
     {
+        // Tester at lagerbeholdningen reduceres med 1 når bartenderen registrerer et salg
+        [Fact]
+        public void RegisterSale_ReducesStock_WhenUserRoleIsBartender()
+        {
+            var bartender = new User { Role = UserRole.Bartender };
+            var product = new Product
+            {
+                Name = "Beer",
+                Stock = 10,
+                Type = DrinkType.Beer,
+                Price = 25.0m
+            };
+
+            var inventoryService = new InventoryService();
+            inventoryService.RegisterSale(product);
+
+            Assert.Equal(9, product.Stock);
+        }
+
+        // Tester at lagerbeholdningen IKKE reduceres når rollen er null (ikke logget ind)
+        [Fact]
+        public void RegisterSale_DoesNotReduceStock_WhenUserRoleIsNull()
+        {
+            var noRoleUser = new User { Role = null };
+            var product = new Product
+            {
+                Name = "Beer",
+                Stock = 10,
+                Type = DrinkType.Beer,
+                Price = 25.0m
+            };
+
+            var inventoryService = new InventoryService();
+            inventoryService.RegisterSale(product);
+
+            Assert.Equal(10, product.Stock);
+        }
     }
 }
