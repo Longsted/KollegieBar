c0bd8ed43a2ab82ff4815e27ea6cc6553779e1ed
Bod1l <lasse.dahm@hotmail.dk>
2026-04-09 10:10:02 +0200

    User storie 15 - Task 173 Refined User class

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -3,5 +3,15 @@
 public class User
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public UserRoles Role { get; set; }
+
+    public User(string username, string password, UserRoles role)
+    {
+        UserName = username;
+        Password = password;
+        Role = role;
+    }
+    
 }
