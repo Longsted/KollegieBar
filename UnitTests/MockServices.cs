952f803ef05d330c7bad4b9edf01530059da51d6
kevin <eaa24ksra@students.eaaa.dk>
2026-04-14 09:11:03 +0200

    Implemented a new MockServices that would be necessary for unittests, some were already in mockmodels and were moved to this new class.
    UserStory3 class has been added and userstory 1 and 2 have been updated to follow this new mockservices format instead.

---
+++ UnitTests/MockServices.cs
@@ -0,0 +1,32 @@
+﻿namespace UnitTests
+{
+    public class SaleService
+    {
+        public bool RegisterSale(User user, Product product)
+        {
+            if (user?.Role != UserRole.Bartender)
+                return false;
+
+            product.Stock -= 1;
+            return true;
+        }
+    }
+
+    public class SalesStatisticsService
+    {
+        private readonly List<Product> _salesData;
+
+        public SalesStatisticsService(List<Product> salesData)
+        {
+            _salesData = salesData;
+        }
+
+        public List<Product> GetStatistics(User user)
+        {
+            if (user?.Role != UserRole.BoardMember)
+                return new List<Product>();
+
+            return _salesData;
+        }
+    }
+}
--- UnitTests/UserStory2.cs
+++ UnitTests/UserStory2.cs
@@ -3,40 +3,38 @@ using System.Collections.Generic;
 
 namespace UnitTests
 {
-    public class UserStory2Tests
+    public class UserStory2
     {
-        // Tester at statistik over salget sendes når bestyrelsesmedlemmet har korrekt rolle
         [Fact]
-        public void GetSalesStatistics_ReturnsData_WhenUserRoleIsBoardMember()
+        public void GetStatistics_ReturnsData_WhenBoardMemberIsLoggedIn()
         {
-            var boardMember = new User { Role = UserRole.BoardMember };
+            var user = new User { Role = UserRole.BoardMember };
 
             var salesData = new List<Product>
             {
-                new Product { Name = "Beer", Stock = 10, Type = DrinkType.Beer, Price = 25.0m },
-                new Product { Name = "Cider", Stock = 5, Type = DrinkType.Cider, Price = 30.0m }
+                new Product { Name = "Beer", Stock = 10 },
+                new Product { Name = "Cider", Stock = 5 }
             };
 
-            var statisticsService = new StatisticsService(salesData);
-            var result = statisticsService.GetSalesStatistics();
+            var service = new SalesStatisticsService(salesData);
+            var result = service.GetStatistics(user);
 
             Assert.NotEmpty(result);
         }
 
-        // Tester at statistik IKKE sendes når rollen er null (ikke logget ind)
         [Fact]
-        public void GetSalesStatistics_ReturnsEmpty_WhenUserRoleIsNull()
+        public void GetStatistics_ReturnsEmpty_WhenUserIsNotLoggedIn()
         {
-            var noRoleUser = new User { Role = null };
+            var user = new User { Role = null };
 
             var salesData = new List<Product>
             {
-                new Product { Name = "Beer", Stock = 10, Type = DrinkType.Beer, Price = 25.0m },
-                new Product { Name = "Cider", Stock = 5, Type = DrinkType.Cider, Price = 30.0m }
+                new Product { Name = "Beer", Stock = 10 },
+                new Product { Name = "Cider", Stock = 5 }
             };
 
-            var statisticsService = new StatisticsService(salesData);
-            var result = statisticsService.GetSalesStatistics();
+            var service = new SalesStatisticsService(salesData);
+            var result = service.GetStatistics(user);
 
             Assert.Empty(result);
         }
