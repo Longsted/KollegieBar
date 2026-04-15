3f7d432e8170d91f0c6efbf483574c21d90dbe97
Jeppe <eaajkf24@students.eaaa.dk>
2026-04-15 08:40:10 +0200

    createProduct metode

--- FrontEnd/CreateProductPage.xaml.cs
+++ FrontEnd/CreateProductPage.xaml.cs
@@ -1,19 +1,39 @@
 using BusinessLogic.BusinessLogicLayer;
+using Data.Context;
 using DataTransferObject.Model;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace FrontEnd;
 
 public partial class CreateProductPage : ContentPage
 {
-    private readonly Product _product;
+    private readonly ProductBusinessLogicLayer _productBLL;
     
     public CreateProductPage()
     {
         InitializeComponent();
-
-        _product = new Product();
         
-        
+        var context = AppDbContext
 
     }
+
+    private void OnCreateProduct_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            
+        }
+        catch (FormatException)
+        {
+            ResultLabel.Text = "Make sure every textfield is filled";
+            throw;
+        }
+        catch (Exception ex)
+        {
+            ResultLabel.Text = $"Fejl: {ex.Message}";
+        }
+    }
+    
+    
+    
 }
