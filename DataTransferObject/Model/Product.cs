092a7a4e1533f76f9915d016aa464b984a75fd07
Niels <eaa24nlgl@students.eaaa.dk>
2026-04-15 09:33:49 +0200

    nyevarenumre/task154 (#29)
    * Add product inheritance, migration and validation
    
    * Add/update SQL queries

--- DataTransferObject/Model/Product.cs
+++ DataTransferObject/Model/Product.cs
@@ -3,7 +3,7 @@
 public abstract class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } 
     public decimal CostPrice{ get; set; }
     public int StockQuantity{ get; set; }
 
