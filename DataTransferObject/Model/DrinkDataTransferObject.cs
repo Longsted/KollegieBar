612ca539d72eb8678320fc00712dca1d023be8da
Bod1l <lasse.dahm@hotmail.dk>
2026-04-20 11:47:09 +0200

    After merge conflicts

---
+++ DataTransferObject/Model/DrinkDataTransferObject.cs
@@ -0,0 +1,24 @@
+﻿namespace DataTransferObject.Model;
+
+public class DrinkDataTransferObject
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public double CostPrice { get; set; }
+    public double SalesPrice { get; set; }
+
+    public bool IsAlcoholic { get; set; }
+
+    public List<DrinkIngredientDataTransferObject> Ingredients { get; set; } = new();
+
+    public DrinkDataTransferObject() { }
+
+    public DrinkDataTransferObject(string name, double costPrice, double salesPrice, bool isAlcoholic)
+    {
+        Name = name;
+        CostPrice = costPrice;
+        SalesPrice = salesPrice;
+        IsAlcoholic = isAlcoholic;
+    }
+}
