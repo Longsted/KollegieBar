f76408adc022e2e449d720601718a23ad1d1ca0d
André Eickmayer Hansen <andr265i@gmail.com>
2026-04-20 10:16:42 +0200

    Changed DataTransferObjectModel Drink to DrinkDTO and added new Overloaded constructor for when we want to create a sale for a drink and Added a Migration

--- DataTransferObject/Model/DrinkIngredient.cs
+++ DataTransferObject/Model/DrinkIngredient.cs
@@ -7,7 +7,7 @@ public class DrinkIngredient
     public int Id { get; set; }
 
     public int DrinkId { get; set; }
-    public Drink Drink { get; set; }
+    public DrinkDto Drink { get; set; }
 
     public int LiquidProductId { get; set; }
     public LiquidProductDto LiquidProductDto { get; set; }
