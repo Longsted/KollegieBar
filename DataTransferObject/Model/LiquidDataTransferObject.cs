63db51cd4d55757912fc2f73bf4fdbf523c714a6
Bod1l <lasse.dahm@hotmail.dk>
2026-04-20 12:11:01 +0200

    Changes again

--- DataTransferObject/Model/LiquidDataTransferObject.cs
+++ DataTransferObject/Model/LiquidDataTransferObject.cs
@@ -3,7 +3,7 @@
 public class LiquidDataTransferObject : ProductDto
 {
     public int VolumeCl { get; set; }
-    public double Pant { get; set; }
+    public Pant Pant { get; set; }
     public double AlcoholPercentage { get; set; }
     public bool SugarFree { get; set; }
 
@@ -11,14 +11,14 @@ public class LiquidDataTransferObject : ProductDto
     {
     }
 
-    public LiquidDataTransferObject(string name, decimal costPrice, int stockQuantity, int volumeCl, double pant, double alcoholPercentage) : base(name, costPrice, stockQuantity)
+    public LiquidDataTransferObject(string name, decimal costPrice, int stockQuantity, int volumeCl, Pant pant, double alcoholPercentage) : base(name, costPrice, stockQuantity)
     {
         VolumeCl = volumeCl;
         Pant = pant;
         AlcoholPercentage = alcoholPercentage;
     }
 
-    public LiquidDataTransferObject(string name, decimal costPrice, int stockQuantity, int volumeCl, double pant, bool sugarFree) : base(name, costPrice, stockQuantity)
+    public LiquidDataTransferObject(string name, decimal costPrice, int stockQuantity, int volumeCl, Pant pant, bool sugarFree) : base(name, costPrice, stockQuantity)
     {
         VolumeCl = volumeCl;
         Pant = pant;
