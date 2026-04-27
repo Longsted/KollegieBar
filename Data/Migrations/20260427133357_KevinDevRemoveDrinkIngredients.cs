485ed3064f11f068b338586121e5995a89598183
kevin <eaa24ksra@students.eaaa.dk>
2026-04-27 15:43:01 +0200

    Removed Drink Ingredient and fixed some stuff for front end.

---
+++ Data/Migrations/20260427133357_KevinDevRemoveDrinkIngredients.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class KevinDevRemoveDrinkIngredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DrinkId",
+                table: "Products",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "DrinkId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_DrinkId",
+                table: "Products",
+                column: "DrinkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Drinks_DrinkId",
+                table: "Products",
+                column: "DrinkId",
+                principalTable: "Drinks",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Drinks_DrinkId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_DrinkId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DrinkId",
+                table: "Products");
+        }
+    }
+}
